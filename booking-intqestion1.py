148d247e6050316820314412c9383cd2b8ca7939
Valentyn Chubukin <val@valch.name>
2022-02-18 18:02:32 +0200

    add one more solution

--- booking-intqestion1.py
+++ booking-intqestion1.py
@@ -8,12 +8,12 @@ Site Reliability Engineer Interview
     Given a list of tuples (hotel_id, hotel_name, city) return a list of all "confusing" cities.
 
     Input: [
-      {hotel_1234, "Sheraton", "Amsterdam"} ,
-      {hotel_1000, "Sheraton", "Buenos Aires"} ,
-      {hotel_1001, "Hilton", "Amsterdam"} ,
-      {hotel_1002, "Royal Palace", "Bogota"} ,
-      {hotel_1003, "Hilton", "Amsterdam"} ,
-      {hotel_1004, "Sheraton", "Buenos Aires"} ,
+      {hotel_1234, "Sheraton", "Amsterdam"},
+      {hotel_1000, "Sheraton", "Buenos Aires"},
+      {hotel_1001, "Hilton", "Amsterdam"},
+      {hotel_1002, "Royal Palace", "Bogota"},
+      {hotel_1003, "Hilton", "Amsterdam"},
+      {hotel_1004, "Sheraton", "Buenos Aires"},
       {hotel_1005, "Sheraton", "Buenos Aires"}
     ]
 
@@ -21,26 +21,42 @@ Site Reliability Engineer Interview
 """
 from collections import Counter
 
-Input = [
-("hotel_1234", "Sheraton", "Amsterdam") ,
-("hotel_1000", "Sheraton", "Buenos Aires") ,
-("hotel_1001", "Hilton", "Amsterdam") ,
-("hotel_1002", "Royal Palace", "Bogota") ,
-("hotel_1003", "Hilton", "Amsterdam") ,
-("hotel_1004", "Sheraton", "Buenos Aires") ,
+Input_wrong = [
+("hotel_1234", "Sheraton", "Amsterdam"),
+("hotel_1000", "Sheraton", "Buenos Aires"),
+("hotel_1001", "Hilton", "Amsterdam"),
+("hotel_1002", "Royal Palace", "Bogota"),
+("hotel_1003", "Hilton", "Amsterdam"),
+("hotel_1004", "Sheraton", "Buenos Aires"),
 ("hotel_1005", "Sheraton", "Buenos Aires")
 ]
+
+Input_correct = [
+{"hotel_1234", "Sheraton", "Amsterdam"},
+{"hotel_1000", "Sheraton", "Buenos Aires"},
+{"hotel_1001", "Hilton", "Amsterdam"},
+{"hotel_1002", "Royal Palace", "Bogota"},
+{"hotel_1003", "Hilton", "Amsterdam"},
+{"hotel_1004", "Sheraton", "Buenos Aires"},
+{"hotel_1005", "Sheraton", "Buenos Aires"}
+]
+
+#1ST solution via Counter
 confusing_cities = []
-counter = {}
-whithout_hnumber = []
-"""
-for i,v in Counter((elem[1],elem[2]) for elem in Input).items():
+for i,v in Counter((elem[1],elem[2]) for elem in Input_wrong).items():
     #dict_items([(('Sheraton', 'Amsterdam'), 1), (('Sheraton', 'Buenos Aires'), 3), (('Hilton', 'Amsterdam'), 2), (('Royal Palace', 'Bogota'), 1)])
     if v >= 3:
         confusing_cities.append(i[1])
-"""
+print("1ST solution")
+print(confusing_cities)
+
+
+#2ND solution plain
+confusing_cities = []
+counter = {}
+whithout_hnumber = []
 # get rid of hotel number
-for elem in Input:
+for elem in Input_wrong:
     whithout_hnumber.append(elem[1:])
 # count element whithout_hnumber
 for elem in whithout_hnumber:
@@ -53,5 +69,14 @@ for key in counter:
     #get value of the dict that is amount of same hotels in the same city
     if int(counter[key]) >= 3:
         confusing_cities.append(key[1])
+print("2ND solution")
 print(confusing_cities)
+
+#3D solution via intersection
+intersection = {}
+
+for i in Input_correct:
+    intersection = set.intersection(i)
+print("3D solution")
+print(list(intersection)[1])
 
